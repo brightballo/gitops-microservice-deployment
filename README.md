b9409bac8cb1a2073f8800b6fa4a7c25798882cb
starburn-hub <brightenxxx@gmail.com>
2023-03-05 16:25:27 +0100

    feat: react app

---
+++ README.md
@@ -0,0 +1,1 @@
+# sample-reactapp-with-db
---
+++ demo-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Altschool final semester exam
+      <h1>My Project React App with a mysql database</h1>
+      <p>Welcome :)</p>
+
+      <h2>Start editing to see some magic happen!</h2>
+
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
